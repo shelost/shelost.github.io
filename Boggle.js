2df26f8db9bea789024d8e7a86cd07a086cc48db
shelost <57425238+shelost@users.noreply.github.com>
2019-11-13 21:04:55 -0500

    Add files via upload

---
+++ Boggle.js
@@ -0,0 +1,107 @@
+// JavaScript Document
+
+
+document.getElementById('newBoggle').onclick = function assign(){
+	
+	const charlist = ['A','A','A','A','A', 'B', 'C', 'D', 'E','E','E','E','E', 'F', 'G', 'H', 'I', 'I','I','I','I','J', 'K', 'L', 'M', 'N', 'O', 'O','O','O','O','P', 'Q', 'R', 'S', 'T', 'U','U','U','U','U', 'V', 'W', 'X', 'Y', 'Z'];
+	
+	var num1 = Math.floor(Math.random()*42);
+	var char1 = charlist[num1];
+	document.getElementById('box1').innerHTML = char1;
+	
+	var num2 = Math.floor(Math.random()*42);
+	var char2 = charlist[num2];
+	document.getElementById('box2').innerHTML = char2;
+	
+	var num3 = Math.floor(Math.random()*42);
+	var char3 = charlist[num3];
+	document.getElementById('box3').innerHTML = char3;
+	
+	var num4 = Math.floor(Math.random()*42);
+	var char4 = charlist[num4];
+	document.getElementById('box4').innerHTML = char4;
+	
+	var num5 = Math.floor(Math.random()*42);
+	var char5 = charlist[num5];
+	document.getElementById('box5').innerHTML = char5;
+	
+	var num6 = Math.floor(Math.random()*42);
+	var char6 = charlist[num6];
+	document.getElementById('box6').innerHTML = char6;
+	
+	var num7 = Math.floor(Math.random()*42);
+	var char7 = charlist[num7];
+	document.getElementById('box7').innerHTML = char7;
+	
+	var num8 = Math.floor(Math.random()*42);
+	var char8 = charlist[num8];
+	document.getElementById('box8').innerHTML = char8;
+	
+	var num9 = Math.floor(Math.random()*42);
+	var char9 = charlist[num9];
+	document.getElementById('box9').innerHTML = char9;
+	
+	var num10 = Math.floor(Math.random()*42);
+	var char10 = charlist[num10];
+	document.getElementById('box10').innerHTML = char10;
+	
+	var num11 = Math.floor(Math.random()*42);
+	var char11 = charlist[num11];
+	document.getElementById('box11').innerHTML = char11;
+	
+	var num12 = Math.floor(Math.random()*42);
+	var char12 = charlist[num12];
+	document.getElementById('box12').innerHTML = char12;
+	
+	var num13 = Math.floor(Math.random()*42);
+	var char13 = charlist[num13];
+	document.getElementById('box13').innerHTML = char13;
+	
+	var num14 = Math.floor(Math.random()*42);
+	var char14 = charlist[num14];
+	document.getElementById('box14').innerHTML = char14;
+	
+	var num15 = Math.floor(Math.random()*42);
+	var char15 = charlist[num15];
+	document.getElementById('box15').innerHTML = char15;
+	
+	var num16 = Math.floor(Math.random()*42);
+	var char16 = charlist[num16];
+	document.getElementById('box16').innerHTML = char16;
+	
+	var num17 = Math.floor(Math.random()*42);
+	var char17 = charlist[num17];
+	document.getElementById('box17').innerHTML = char17;
+	
+	var num18 = Math.floor(Math.random()*42);
+	var char18 = charlist[num18];
+	document.getElementById('box18').innerHTML = char18;
+	
+	var num19 = Math.floor(Math.random()*42);
+	var char19 = charlist[num19];
+	document.getElementById('box19').innerHTML = char19;
+	
+	var num20 = Math.floor(Math.random()*42);
+	var char20 = charlist[num20];
+	document.getElementById('box20').innerHTML = char20;
+	
+	var num21 = Math.floor(Math.random()*42);
+	var char21 = charlist[num21];
+	document.getElementById('box21').innerHTML = char21;
+	
+	var num22 = Math.floor(Math.random()*42);
+	var char22 = charlist[num22];
+	document.getElementById('box22').innerHTML = char22;
+	
+	var num23 = Math.floor(Math.random()*42);
+	var char23 = charlist[num23];
+	document.getElementById('box23').innerHTML = char23;
+	
+	var num24 = Math.floor(Math.random()*42);
+	var char24 = charlist[num24];
+	document.getElementById('box24').innerHTML = char24;
+	
+	var num25 = Math.floor(Math.random()*42);
+	var char25 = charlist[num25];
+	document.getElementById('box25').innerHTML = char25;
+}
